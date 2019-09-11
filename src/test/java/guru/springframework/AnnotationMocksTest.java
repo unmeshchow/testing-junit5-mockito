37d9a632dcc81b80582f5f0f377815c02f33eb1c
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-09-11 10:50:15 +0600

    create mock with annotation

---
+++ src/test/java/guru/springframework/AnnotationMocksTest.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+/**
+ * Created by uc on 9/11/2019
+ */
+public class AnnotationMocksTest {
+
+    @Mock
+    Map<String, Object> map;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testMock() {
+        map.put("keyvalue", "foo");
+    }
+}
